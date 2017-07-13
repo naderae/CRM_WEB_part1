f2c6e5675e0d04b3a8de99a9e9bfe5f52c48bbe6
naderae <naderabouezze93@gmail.com>
2017-07-12 21:44:19 -0400

    everything complete

--- crm.rb
+++ crm.rb
@@ -5,3 +5,16 @@ require_relative 'contact.rb'
 get '/' do
   erb :index
 end
+
+get '/contacts' do
+  @all_contacts = Contact.all
+  erb :contacts
+end
+
+get '/about' do
+  erb :about
+end
+
+after do
+  ActiveRecord::Base.connection.close
+end
