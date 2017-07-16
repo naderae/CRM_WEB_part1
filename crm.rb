d0e5b817d9f7c612672eb4e898d05e08026c017a
naderae <naderabouezze93@gmail.com>
2017-07-16 17:22:12 -0400

    done with adding contact

--- crm.rb
+++ crm.rb
@@ -33,6 +33,16 @@ end
 # contact_id = params[:id] # dynamic part of URL is stored in a hash called params, that takes a key which is the id of the contact. remember: hash_name[:key] = value
   # @contact = Contact.find_by({id:contact_id.to_i}) # ==> here, we just pass in the key-value pair for find_by to locate it, and save it as @contact.
 
+  post '/contacts' do
+    Contact.create(
+      first_name: params[:first_name],
+      last_name:  params[:last_name],
+      email:      params[:email],
+      note:       params[:note]
+      )
+      redirect to('/contacts')
+    end  # params hash is directly connected to the input names you gave in new.erb.
+
 
 
 
